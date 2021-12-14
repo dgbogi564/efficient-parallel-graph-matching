e22c866a9093362cba4d909a238aaad1c0021c19
dgbogi564 <82741370+dgbogi564@users.noreply.github.com>
2021-12-13 19:55:57 -0500

    Initial commit

---
+++ src/gpu_required/markFilterEdges.cu
@@ -0,0 +1,14 @@
+/*
+ **********************************************
+ *  CS314 Principles of Programming Languages *
+ *  Fall 2021                                 *
+ **********************************************
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+__global__ void markFilterEdges_gpu(int * src, int * dst, int * matches, int * keepEdges, int numEdges) {
+	/** YOUR CODE GOES BELOW **/
+
+	/** YOUR CODE GOES ABOVE **/
+}
