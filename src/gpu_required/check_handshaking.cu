2e2da23d6b743855653ba75ad2e5a284ed2c22b4
dgbogi564 <82741370+dgbogi564@users.noreply.github.com>
2021-12-14 23:05:41 -0500

    check_handshaking.cu: full implementation

--- src/gpu_required/check_handshaking.cu
+++ src/gpu_required/check_handshaking.cu
@@ -6,9 +6,18 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include "utils.hpp"
 
 __global__ void check_handshaking_gpu(int * strongNeighbor, int * matches, int numNodes) {
 	/** YOUR CODE GOES BELOW **/
-
+    int tid = blockDim.x * blockIdx.x + threadIdx.x;
+    for(int node = tid; node < numNodes; node += blockDim.x * gridDim.x) {
+        int strongestNeighbor = strongNeighbor[node];
+        if(strongestNeighbor < 0 || matches[node] >= 0) continue;
+        if(strongNeighbor[strongestNeighbor] == node) {
+            matches[node] = strongestNeighbor;
+            matches[strongestNeighbor] = node;
+        }
+    }
 	/** YOUR CODE GOES ABOVE **/
 }
